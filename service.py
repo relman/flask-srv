c239507722384384942069d57e6b3cb314e6bbcf
Flint Storman <flint.storman@gmail.com>
2016-08-26 12:47:50 +0600

    updated accordingly to a FlaskWebServiceUpwork.doc

--- service.py
+++ service.py
@@ -1,29 +1,33 @@
 import gnupg
-import json
 import urllib
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/createCsr', methods=['GET'])
+
+@app.route('/decryptMessage', methods=['GET'])
 def create_csr_get():
     message = urllib.unquote(request.args.get('Message') or '')
-    passphrase = urllib.unquote(request.args.get('PassPhrase') or '')
+    passphrase = urllib.unquote(request.args.get('Passphrase') or '')
     return decrypt(message, passphrase)
 
-@app.route('/createCsr', methods=['POST'])
+
+@app.route('/decryptMessage', methods=['POST'])
 def create_csr_post():
     message = request.json.get('Message', None)
-    passphrase = request.json.get('PassPhrase', None)
+    passphrase = request.json.get('Passphrase', None)
     return decrypt(message, passphrase)
-    
+
+
 def decrypt(message, passphrase):
     if not passphrase or not message:
-        return jsonify({'Error': 'PassPhrase and Message are required'})
+        return jsonify({'Error': 'Passphrase and Message are required'}), 400
     gpg = gnupg.GPG()
     result = gpg.decrypt(message, passphrase=passphrase)
     if not result.ok:
-        return jsonify({'Error': 'Unable to decrypt message'})
+        return jsonify({'Error': 'Unable to decrypt message'}), 400
     return jsonify({'DecryptedMessage': str(result)})
+
 
 if __name__ == '__main__':
     app.run()
--- service_tests.py
+++ service_tests.py
@@ -3,27 +3,33 @@ import json
 import unittest
 import urllib
 
+
 class ServiceTestCase(unittest.TestCase):
-    root_url = '/createCsr'
-    encrypted = '-----BEGIN PGP MESSAGE-----\nVersion: GnuPG v1\n\njA0EAwMCfq/WT2Po8I1gyVVNDJLvVoLteW1fZ/NvCPt39ieT/ZqfSpAas2KyK2tK\nuAf7cMtNynrbwR7K6gHD82GrfIHxKqKQX/+E1Q+UHU+WIdU1KdL3Xe4Uxb2sMVeE\nRD+VSmRs\n=FaJd\n-----END PGP MESSAGE-----\n'
+    root_url = '/decryptMessage'
+    encrypted = '-----BEGIN PGP MESSAGE-----\n' \
+                'Version: GnuPG v1\n\n' \
+                'jA0EAwMCfq/WT2Po8I1gyVVNDJLvVoLteW1fZ/NvCPt39ieT/ZqfSpAas2KyK2tK\n' \
+                'uAf7cMtNynrbwR7K6gHD82GrfIHxKqKQX/+E1Q+UHU+WIdU1KdL3Xe4Uxb2sMVeE\n' \
+                'RD+VSmRs\n=FaJd\n' \
+                '-----END PGP MESSAGE-----\n'
 
     def setUp(self):
         service.app.config['TESTING'] = True
         self.app = service.app.test_client()
 
     def send_get(self, message, passphrase):
-        return self.app.get(self.root_url+'?Message='+message+'&PassPhrase='+passphrase)
+        return self.app.get(self.root_url + '?Message=' + message + '&Passphrase=' + passphrase)
 
     def send_post(self, data):
-        return self.app.post(self.root_url, data=json.dumps(data), 
-                content_type='application/json')
+        return self.app.post(self.root_url, data=json.dumps(data),
+                             content_type='application/json')
 
     def test_send_get_no_message(self):
-        rv = self.app.get(self.root_url+'?PassPhrase=123')
+        rv = self.app.get(self.root_url + '?Passphrase=123')
         assert 'Error' in rv.data
 
     def test_send_get_no_passphrase(self):
-        rv = self.app.get(self.root_url+'?Message=text')
+        rv = self.app.get(self.root_url + '?Message=text')
         assert 'Error' in rv.data
 
     def test_send_get_wrong_passphrase(self):
@@ -56,11 +62,12 @@ class ServiceTestCase(unittest.TestCase):
 
     def test_send_post_correct_params(self):
         rv = self.send_post(dict(
-            Message=self.encrypted, 
-            PassPhrase='asdf'
+            Message=self.encrypted,
+            Passphrase='asdf'
         ))
         assert 'DecryptedMessage' in rv.data
         assert 'Flask is a micro webdevelopment framework' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
